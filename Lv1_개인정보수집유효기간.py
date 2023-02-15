cf3d8bfcf812257e884b229e0e858f080678c315
jeongmigwang <anekd1917@naver.com>
2023-02-15 10:29:56 +0900

    성격유형검사하기(수정) + 신고결과받기(풀이) + 개인정보수집유효기간(추가)

---
+++ Lv1_개인정보수집유효기간.py
@@ -0,0 +1,13 @@
+def solution(id_list,report,k):
+    answer = []
+    return answer
+
+# id_list = ["muzi", "frodo", "apeach", "neo"]
+# report = ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"]
+# k = 2
+
+id_list = ["con", "ryan"]
+report = ["ryan con", "ryan con", "ryan con", "ryan con"]
+k = 3
+
+print(solution(id_list,report,k))
--- Lv1_성격유형검사하기.py
+++ Lv1_성격유형검사하기.py
@@ -1,89 +1,28 @@
-survey = list(input("Input survay: >>").split(', '))
-i = 0
-for sur in survey:
-    sur=sur.replace("\"","").replace('[','').replace(']','')
-    survey[i] = sur
-    i += 1
+def solution(survey, choiced):
+    answer = ''
+    score_list = {'R':0,'T':0,'C':0,'F':0,'J':0,'M':0,'A':0,'N':0}
+    for i in range(len(choices)):
+        if choices[i] >= 4:
+            score_list[survey[i][1]]+=choices[i]-4
+        else:
+            score_list[survey[i][0]]+=4-choices[i]
 
-choices = list(input("Input choices: >>").split(', '))
-i = 0
-for cho in choices:
-    cho=cho.replace('[','').replace(']','')
-    choices[i] = int(cho)
-    i+=1
+    #점수 더 높은 거 저장   
+    answer += 'R' if score_list['R']>=score_list['T'] else 'T'
+    answer += 'C' if score_list['C']>=score_list['F'] else 'F'
+    answer += 'J' if score_list['J']>=score_list['M'] else 'M'
+    answer += 'A' if score_list['A']>=score_list['N'] else 'N'
+    
+    
+    return answer
 
-result = []
-for i in range(4):
-    line=[]
-    for j in range(2):
-        line.append(0)
-    result.append(line)
+survey = ["AN", "CF", "MJ", "RT", "NA"]
+choices = [5, 3, 2, 7, 5]
 
+# survey = ["TR", "RT", "TR"]
+# choices = [7, 1, 3]
 
-for i in range(len(choices)):
-    s = survey[i][0]
-    if(s=='R'):
-        if(choices[i]<4):
-            result[0][0] += 4-choices[i]
-        elif(choices[i]>4):
-            result[0][1] += choices[i]-4
-        else:
-            continue
-    elif(s=='T'):
-        if(choices[i]<4):
-            result[0][1] += 4-choices[i]
-        elif(choices[i]>4):
-            result[0][0] += choices[i]-4
-        else:
-            continue
-    elif(s=='C'):
-        if(choices[i]<4):
-            result[1][0] += 4-choices[i]
-        elif(choices[i]>4):
-            result[1][1] += choices[i]-4
-        else:
-            continue    
-    elif(s=='F'):
-        if(choices[i]<4):
-            result[1][1] += 4-choices[i]
-        elif(choices[i]>4):
-            result[1][0] += choices[i]-4
-        else:
-            continue    
-    elif(s=='J'):
-        if(choices[i]<4):
-            result[2][0] += 4-choices[i]
-        elif(choices[i]>4):
-            result[2][1] += choices[i]-4
-        else:
-            continue
-    elif(s=='M'):
-        if(choices[i]<4):
-            result[2][1] += 4-choices[i]
-        elif(choices[i]>4):
-            result[2][0] += choices[i]-4
-        else:
-            continue
-    elif(s=='A'):
-        if(choices[i]<4):
-            result[3][0] += 4-choices[i]
-        elif(choices[i]>4):
-            result[3][1] += choices[i]-4
-        else:
-            continue
-    else: #if(s=='N')  
-        if(choices[i]<4):
-            result[3][1] += 4-choices[i]
-        elif(choices[i]>4):
-            result[3][0] += choices[i]-4
-        else:
-            continue
-res=""
-list =[['R','T'],['C','F'],['J','M'],['A','N']]
-for i in range(4):
-    if(result[i][0]<result[i][1]):
-        res+=(list[i][1])
-    else:
-        res+=(list[i][0])
+# survey = []
+# choices =[]
 
-print(res)
+print(solution(survey, choices))
