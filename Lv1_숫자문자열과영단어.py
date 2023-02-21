3927501841a712617c5d04e75b14224806fda896
jeongmigwang <anekd1917@naver.com>
2023-02-21 17:23:17 +0900

    Create Lv1_숫자문자열과영단어.py

---
+++ Lv1_숫자문자열과영단어.py
@@ -0,0 +1,11 @@
+def solution(s):
+
+    answer = s.replace('zero','0').replace('one','1').replace('two','2').replace('three','3').replace('four','4').replace('five','5').replace('six','6').replace('seven','7').replace('eight','8').replace('nine','9')
+    return int(answer)
+
+# s="one4seveneight"
+# s="23four5six7"
+# s="2three45sixseven"
+s="123"
+
+print(solution(s))
